13716045b414fa166647624004a65f944b088581
nomadbuzz <baz.coding@gmail.com>
2024-06-11 08:53:27 -0400

    new logo

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios"
+import { food_list } from "../assets/assets";
+
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) =>{
